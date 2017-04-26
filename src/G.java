7e71bd48875e01b76d06abe4478280441de627a0
lngonzalezm <lngonzalezm@unal.edu.co>
2017-04-26 08:54:35 -0500

    Add files via upload

---
+++ src/G.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OtroPunto;
+
+/**
+ *
+ * @author USER
+ */
+public class G { // Esta es la clase Padre
+    public String toString(){
+        return "G";
+    }
+    
+    public void method1(){
+        System.out.print("G 1");
+    }
+    
+    public void method2(){
+        System.out.print("G 2");
+    }
+}
